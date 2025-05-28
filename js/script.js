4a3bf6040fcaaffc6834726883726af99c8332d4
Samuel Dias Lira Xavier Silva <samuel.dsilva11@aluno.edu.es.gov.br>
2025-05-28 17:28:45 -0300

    sla

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const equipe = [
     { nome: "Coordenadora Rita", cargo: "Coordenadora", foto: "../img/rita.jpg" },
     { nome: "Coordenadora Marilza", cargo: "Coordenadora", foto: "../img/marilza.jpg" },
-    { nome: "Pedagoga Simone", cargo: "Pedagoga", foto: "../img/simone.jpg" },
+    { nome: "Pedagoga Simone", cargo: "Pedagoga", foto: "../img/simone.jpeg" },
     { nome: "Coordenadora Pedagógica Claudia", cargo: "Coord. Pedagógica", foto: "../img/.jpg" },
-    { nome: "Diretor Vagner", cargo: "Diretor", foto: "../img/vagner.jpg" }
+    { nome: "Diretor Vagner", cargo: "Diretor", foto: "../img/Vagner.jpeg" }
     // Adicione mais membros da equipe se quiser
   ];
 
